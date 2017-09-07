784247ebdacb87a417041a1f91a23acece9c077a
shamrat17 <shamrat3628@gmail.com>
2017-09-07 15:26:09 +0600

    testing app is ready now

---
+++ app/src/main/java/com/chatbuzz/shamratakbar/myapplication/UserDetails.java
@@ -0,0 +1,11 @@
+package com.chatbuzz.shamratakbar.myapplication;
+
+/**
+ * Created by Shamrat Akbar on 07/09/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
